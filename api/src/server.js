814f4f5b52a16ade26d9a2cd5768cf3fef6fe16b
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-08-15 17:07:42 -0300

    cambio de msj

--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hola mundo!");
 });
 
 module.exports = server;
