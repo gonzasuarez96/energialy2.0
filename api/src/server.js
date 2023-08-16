5a46e927c6814153b8c448c751c0a44ed7b3c9da
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-08-16 09:43:17 -0300

    Hello world

--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("Hola mundo!");
+  res.send("Hello World!");
 });
 
 module.exports = server;
