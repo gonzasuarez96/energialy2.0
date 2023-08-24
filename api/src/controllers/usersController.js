71ad1e0341b5abae289ac2701e0a243b7b3d1831
unknown <Jorgemathez14@gmail.com>
2023-08-24 14:52:55 -0300

    userControl

--- api/src/controllers/usersController.js
+++ api/src/controllers/usersController.js
@@ -1,9 +1,46 @@
 const { Users } = require('../db');
 
+const getAllUsers = async () => {
+  const allUsers = await Users.findAll();
+  /*
+  [
+    { id: '1', email: 'user1@example.com', isActive: true },
+    { id: '2', email: 'user2@example.com', isActive: false }
+  ];
+*/
+  return allUsers;
+};
 
+const getUserByID = async (id) => {
+  const user = await Users.findByPk(id);
+  if (!user) {
+    const error = new Error(`User with id ${id} not found.`);
+    error.status = 404;
+    throw error;
+  }
+  return user;
+};
 
-
+const updateUserProfile = async (id, newData) => {
+  const user = await Users.findByPk(id);
+  if (!user) {
+    const error = new Error(`User with id ${id} not found.`);
+    error.status = 404;
+    throw error;
+  }
+  
+  // actualizo los datos del perfil del usuario con newData
+  await user.update(newData);
+  
+  return user;
+};
 
 module.exports = {
+  getAllUsers,
+  getUserByID,
+  updateUserProfile
+};
 
-}
+
+/* get traer usuarios por id 
+y un put para editar el perfil del usuario */
--- api/src/routes/resources/usersRouter.js
+++ api/src/routes/resources/usersRouter.js
@@ -1,18 +1,17 @@
 const { Router } = require('express');
 const {
-  // getUsersHandler,
-  // getUserByIDHandler,
+  getUsersHandler,
+   getUserByIDHandler,
+   updateUserProfileHandler,
   // createUserHandler,
-  // updateUserHandler,
-  // deleteUserHandler
+  // deleteUserHandler,
 } = require('../../handlers/usersHandler');
 
 const usersRouter = Router();
 
-// usersRouter.get('/', getUsersHandler);
-// usersRouter.get('/:id', getUserByIDHandler);
-// usersRouter.post('/', createUserHandler);
-// usersRouter.put('/:id', updateUserHandler);
+usersRouter.get('/', getUsersHandler);
+usersRouter.get('/:id', getUserByIDHandler);
+usersRouter.put('/:id', updateUserProfileHandler);
 // usersRouter.delete('/:id', deleteUserHandler);
 
 module.exports = usersRouter;
