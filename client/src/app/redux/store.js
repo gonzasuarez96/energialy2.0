bb3e8ca8c8d5025d636c52106a1b838c6f06ab12
gonzasuarez96 <gonzasuarez100@gmail.com>
2023-08-31 16:54:16 -0300

    modificacion a redux toolkit

--- client/src/app/redux/store.js
+++ client/src/app/redux/store.js
@@ -1,13 +1,8 @@
-'use client'
-import { createStore, applyMiddleware, compose } from "redux";
-import reducer from "./reducer";
-import thunkMiddleware from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+import Slice from './slice';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-    reducer,
-    composeEnhancer(applyMiddleware(thunkMiddleware))
-);
-
-export default store;
+export default configureStore({
+    reducer:{
+        auth: Slice.reducer
+    }
+})
