b7a97ba313883f6b3f9adf683ddcb1870f253c5c
fspiritosi <fspiritosi@gmail.com>
2023-09-07 10:39:15 -0300

    sideBar funcionando, y navegando

---
+++ client/src/app/dashboard/components/MenuItem.jsx
@@ -0,0 +1,48 @@
+'use client'
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+
+function MenuItem({menuItem, index, isOpen}) {
+   const [subMenuOpen, setSubMenuOpen] = useState(false);
+   const router = useRouter();
+  return (
+    <>
+      <li
+        key={index}
+        className={`text-gray-800 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-slate-200 rounded-md ${
+          menuItem.spacing ? "mt-9" : "mt-2"
+        }`}
+        onClick={()=> {if(!menuItem.submenu){ router.push(`${menuItem.url}`)}}}
+      >
+        <span className="text-2xl block float-left">{menuItem.icon}</span>
+        <span className={`text-base font-medium flex-1 ${!isOpen && "hidden"}`}>
+          {menuItem.title}
+        </span>
+        {menuItem.submenu && isOpen && (
+          <MdOutlineArrowDropDown
+            className={`${subMenuOpen && "rotate-180 duration-300"}`}
+            onClick={() => {
+              setSubMenuOpen(!subMenuOpen);
+            }}
+          />
+        )}
+      </li>
+      {menuItem.submenu && isOpen && subMenuOpen && (
+        <ul>
+          {menuItem.submenuItems.map((item, index) => (
+            <li
+              key={index}
+              className={`text-gray-800 text-xs flex items-center gap-x-4 cursor-pointer p-1 px-3 hover:bg-slate-200 rounded-md`}
+              onClick={() => router.push(`${item.url}`)}
+            >
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default MenuItem
