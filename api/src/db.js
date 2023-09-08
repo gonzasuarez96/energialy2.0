dd07796eeaffe408e5b06e0e331603670a672d9f
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-09-08 20:22:00 -0300

    added dialectModule require pg

--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { POSTGRES_URL } = process.env;
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DATABASE } = process.env;
 
-const sequelize = new Sequelize(POSTGRES_URL, {
+const sequelize = new Sequelize(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DATABASE}?sslmode=require`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectModule: require('pg')
 });
 const basename = path.basename(__filename);
 
