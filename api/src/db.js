2b901d8f493099d0e48c73c973d44dd1350611b2
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-09-08 18:49:07 -0300

    vercel.json + POSTGRES_URL env

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { POSTGRES_URL } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/energialy`, {
+const sequelize = new Sequelize(POSTGRES_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
