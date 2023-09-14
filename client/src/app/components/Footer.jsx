d5d8c6ecdf7d354062d1dc3381d9d53547fbfe37
gonzasuarez96 <gonzasuarez100@gmail.com>
2023-09-14 11:55:03 -0300

    funcionalidad y diseño del footer

--- client/src/app/components/Footer.jsx
+++ client/src/app/components/Footer.jsx
@@ -1,61 +1,96 @@
 import React from "react";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { BsLinkedin } from "react-icons/bs";
+
+const oportunities = [
+  { name: "Crear licitaciones", link: "/tenders" },
+  { name: "Participar en Licitaciones", link: "/" },
+  { name: "Financiamiento", link: "/" },
+  { name: "Directorio Energetico", link: "directory" },
+];
 
 export default function Footer() {
   return (
-    <footer
-      className="relative bg-cover bg-center"
-      style={{
-        backgroundImage:
-          'url("https://energialy.ar/uploads/settings/footer/1624496061-Energialy-Banner-footer-2-byn.jpg")',
-        backgroundPosition: "center 60%",
-        height: "300px",
-      }}
-    >
-      <div className="absolute inset-0 bg-gradient-to-r from-violet-500 to-yellow-300 opacity-80"></div>
-      <div className="pt-20 relative z-10 w-full px-4 mx-auto">
-        <div className="flex flex-wrap -mx-4">
-          <div className="w-full sm:w-6/12 md:w-6/12 lg:w-6/12 xl:w-3/12 px-4">
-            <img
-              src="https://energialy.ar/uploads/settings/footer/1624496061-Energialy-Logo-150.png"
-              alt="company logo here"
-            />
+    <footer className="relative bg-[#545353] bg-cover bg-center h-full">
+      <div className="h-full flex items-center justify-center p-2 mx-4 mb-4 mt-12 border-b border-[#3D3D3D]">
+        <div className="flex flex-wrap">
+          <div className="w-full sm:w-6/12 md:w-6/12 lg:w-6/12 xl:w-3/12 px-4 text-[#B4B4B4]">
+            <a href="/">
+              <img
+                src="https://energialy.ar/uploads/settings/footer/1624496061-Energialy-Logo-150.png"
+                alt="company logo here"
+                className="w-60"
+              />
+            </a>
             <div>
-              <p>La Plataforma de Licitaciones Fullstream de Vaca Muerta. Las herramientas de Energialy colaboran en tus procesos licitatorios.</p>
+              <p className="text-base mt-4">
+                La Plataforma de Licitaciones Fullstream de Vaca Muerta. Las
+                herramientas de Energialy colaboran en tus procesos
+                licitatorios.
+              </p>
             </div>
-            <ul>
+          </div>
+          <div className="w-full sm:w-6/12 md:w-3/12 lg:w-3/12 xl:w-3/12 px-4 text-[#B4B4B4]">
+            <h4 className="text-xl tracking-wider mb-4">OPORTUNIDADES</h4>
+            <ul className="pl-0">
+              {oportunities.map((option, index) => (
+                <li key={index} className="pb-2">
+                  <a
+                    href={option.link}
+                    className="no-underline text-current hover:text-primary-500 transition duration-300"
+                  >
+                    {option.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="w-full sm:w-6/12 md:w-3/12 lg:w-3/12 xl:w-3/12 px-4 text-[#B4B4B4]">
+            <h4 className="text-xl tracking-wider mb-4">COMPAÑIA</h4>
+            <ul className="pl-0">
               <li>
-                <a href="https://twitter.com/energialy"><i className="fa fab fa-twitter"></i></a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/company/energialy/"><i className="fa fab fa-google-plus-g"></i></a>
+                <a
+                  href="/register"
+                  className="no-underline text-current hover:text-primary-500 transition duration-300"
+                >
+                  Descubri Energialy
+                </a>
               </li>
             </ul>
           </div>
-          <div className="w-full sm:w-6/12 md:w-3/12 lg:w-3/12 xl:w-3/12 px-4">
-            <h4>OPORTUNIDADES</h4>
-            <ul className="text-white">
-              <li>
-                <a href="#" className="no-underline text-current hover:text-[#191654]">Financiamiento</a>
+          <div className="w-full sm:w-6/12 md:w-3/12 lg:w-3/12 xl:w-3/12 px-4 text-[#B4B4B4]">
+            <h4 className="text-xl tracking-wider mb-4">Seguinos</h4>
+            <ul className="flex list-none p-0">
+              <li className="p-2 mx-2">
+                <a
+                  href="https://twitter.com/energialy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <AiOutlineTwitter
+                    className="text-[#B4B4B4] hover:bg-primary-500 hover:text-white transition duration-300"
+                    size="30px"
+                  ></AiOutlineTwitter>
+                </a>
               </li>
-            </ul>
-          </div>
-          <div className="w-full sm:w-6/12 md:w-3/12 lg:w-3/12 xl:w-3/12 px-4">
-            <h4>COMPAÑIA</h4>
-            <ul className="text-white"> 
-              <li>
-                <a href="#" className="no-underline text-current hover:text-[#191654]">Descubri Energialy</a>
-              </li>
-            </ul>
-          </div>
-          <div className="w-full sm:w-6/12 md:w-3/12 lg:w-3/12 xl:w-3/12 px-4">
-            <h4>Seguinos</h4>
-            <ul className="text-white">
-              <li>
-                <a href="#" className="no-underline text-current hover:text-[#191654]">logo redes</a>
+              <li className="p-2 mx-2">
+                <a
+                  href="https://www.linkedin.com/company/energialy/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <BsLinkedin
+                    className="text-[#B4B4B4] hover:bg-primary-500 hover:text-white transition duration-300"
+                    size="30px"
+                  ></BsLinkedin>
+                </a>
               </li>
             </ul>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center p-4 m-4 text-base text-[#B4B4B4] tracking-wide">
+        <span>2023 © | ENERGIALY SAS | Hecho en Neuquén</span>
       </div>
     </footer>
   );
