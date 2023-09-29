15cc4ccb9ce8b2ffa4120cc102a90c5ffd2d5832
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2023-09-29 19:52:01 -0300

    Update SideBar.jsx

--- client/src/app/dashboard/components/SideBar.jsx
+++ client/src/app/dashboard/components/SideBar.jsx
@@ -49,11 +49,11 @@ function SideBar() {
       </div>
       {/*Company Data */}
       <div className="flex flex-col items-center justify-center">
-        {banner ? <img className="-z-0" src={banner} alt="bannerProfile"/> : null}
-        {logo ? <img
+{/*         {banner ? <img className="-z-0" src={banner} alt="bannerProfile"/> : null} */}
+{/*         {logo ? <img
           src={logo}
           className={`${isOpen ? "w-[100px]" : "w-[50px] duration-300"}`}
-        />: null}
+        />: null} */}
         
       </div>
       {/*Menu Items*/}
