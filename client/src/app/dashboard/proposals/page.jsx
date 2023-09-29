67eceee333d57320a2010fa7cea7fd96c3c52aa7
fspiritosi <fspiritosi@gmail.com>
2023-09-29 19:06:54 -0300

    reparando error de vercel

--- client/src/app/dashboard/proposals/page.jsx
+++ client/src/app/dashboard/proposals/page.jsx
@@ -3,7 +3,7 @@ import ProposalContainer from '@/app/dashboard/proposals/ProposalContainer'
 async function Propuestas() {
   
   const proposals = await fetch('http://localhost:3001/proposals')
-  const proposalsData = await proposals.json()
+  const proposalsData =  proposals.json()
 
   console.log(proposalsData)
   return <ProposalContainer proposals={proposalsData} />;
