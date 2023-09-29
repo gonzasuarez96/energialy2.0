a6f2ed80f8fb05757d53848452210591b0e564d2
fspiritosi <fspiritosi@gmail.com>
2023-09-29 19:09:11 -0300

    cambiando a axios el fetch de proposal

--- client/src/app/dashboard/proposals/page.jsx
+++ client/src/app/dashboard/proposals/page.jsx
@@ -1,8 +1,9 @@
 import ProposalContainer from '@/app/dashboard/proposals/ProposalContainer'
+import axios from 'axios'
 
 async function Propuestas() {
   
-  const proposals = await fetch('http://localhost:3001/proposals')
+  const proposals = await axios.get('http://localhost:3001/proposals')
   const proposalsData =  proposals.json()
 
   console.log(proposalsData)
