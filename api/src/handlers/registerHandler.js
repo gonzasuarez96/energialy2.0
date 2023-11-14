8bbf54cbaf2f1763aec3d2a5754a1e254620611e
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-11-14 19:42:51 -0300

    Fix `role` attribute when creating new user

--- api/src/handlers/registerHandler.js
+++ api/src/handlers/registerHandler.js
@@ -2,8 +2,8 @@ const { newUserRegister } = require('../controllers/registerController');
 
 const newUserRegisterHandler = async (req, res) => {
   try {
-    const { email, password, firstName, lastName } = req.body;
-    const newUser = await newUserRegister(email, password, firstName, lastName);
+    const body = req.body;
+    const newUser = await newUserRegister(body);
     res.status(201).json(newUser);
   } catch (error) {
     res.status(error.status || 500).json({ error: error.message });
