1452253cc30511f0e5c8b9ddb246da5f0dd47747
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-11-14 22:51:21 -0300

    Update `POSTGRES_URL` env variable

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { POSTGRES_URL } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/energialy`, {
+const sequelize = new Sequelize(`${POSTGRES_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -14,13 +14,13 @@ const modelDefiners = [];
 
 // Loading model files from the '/models' directory and pushing their definitions (exported objects) into the modelDefiners array.
 fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
 // Injecting the Sequelize instance into each model definition
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizing the model names for consistency. ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
@@ -74,5 +74,5 @@ FinanceProducts.belongsTo(BankAccounts);
 
 module.exports = {
   ...sequelize.models, // to import models like this: const { Product, User } = require('./db.js');
-  conn: sequelize,     // to import the connection { conn } = require('./db.js');
+  conn: sequelize, // to import the connection { conn } = require('./db.js');
 };
