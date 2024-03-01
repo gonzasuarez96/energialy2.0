18a168af1784ae6a62482da9957aafd3f0e34491
juancruzroldan95 <juancruz.roldan19@gmail.com>
2024-03-01 13:13:37 -0300

    Linter

--- client/src/app/components/Modals/EmailModal.jsx
+++ client/src/app/components/Modals/EmailModal.jsx
@@ -1,83 +1,70 @@
-"use client";
+'use client';
 import { useState } from 'react';
-import { ToastContainer, toast } from "react-toastify";
-import {
-  displaySuccessMessage,
-  displayFailedMessage,
-} from "@/app/components/Toastify";
+import { ToastContainer, toast } from 'react-toastify';
+import { displaySuccessMessage, displayFailedMessage } from '@/app/components/Toastify';
 import axios from 'axios';
 import { urlProduction } from '@/app/data/dataGeneric';
 
 function EmailModal({ open, handleOpen, id, company }) {
-    const [emails, setEmails] = useState([]);
-    const [email, setEmail] = useState('')
-  
-  const endpoint = "inviteCompanies";
-  console.log('companyID',id)
+  const [emails, setEmails] = useState([]);
+  const [email, setEmail] = useState('');
+
+  const endpoint = 'inviteCompanies';
+  console.log('companyID', id);
   const sendObj = {
     companyId: id,
-    emails: emails
-  }
-  
+    emails: emails,
+  };
+
   const handleStatus = async () => {
-    if(emails.length <= 0) {
-      displayFailedMessage("No existen Emails cargados para realizar la invitación");
-    }
-    try {
-      const response = await sendInvitations()
-      displaySuccessMessage("Invitaciones enviadas con exito");
-      setTimeout(() => {
-        handleOpen();
-      }, 2000)
-      
-    } catch (error) {
-      displayFailedMessage("Error al invitar empresas");
+    if (emails.length <= 0) {
+      displayFailedMessage('Agregá al menos un email para enviar la invitación');
+    } else {
+      try {
+        const response = await sendInvitations();
+        displaySuccessMessage('Invitaciones enviadas con exito');
+        setTimeout(() => {
+          handleOpen();
+        }, 3000);
+      } catch (error) {
+        displayFailedMessage('Error al invitar empresas');
+      }
     }
   };
 
   const hanbleEmail = (e) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
   const saveEmail = (e) => {
-    e.preventDefault()
-    setEmails([...emails, email])
-    e.target.reset()
-    setEmail('')
-    
-  }
+    e.preventDefault();
+    setEmails([...emails, email]);
+    e.target.reset();
+    setEmail('');
+  };
 
   const removeEmail = (index) => {
-    const updatedEmails = [...emails]
-    updatedEmails.splice(index, 1)
-    setEmails(updatedEmails)
-  }
+    const updatedEmails = [...emails];
+    updatedEmails.splice(index, 1);
+    setEmails(updatedEmails);
+  };
 
   const sendInvitations = async () => {
-    
-    if (emails.length <= 0) {
-      displayFailedMessage(
-        "No existen Emails cargados para realizar la invitación"
-      );
-    }
-
     try {
       const response = axios.post(`${urlProduction}/${endpoint}`, sendObj);
-      displaySuccessMessage("Invitaciones enviadas con exito");
+      console.log(response.data);
     } catch (error) {
-      displayFailedMessage("Error al invitar empresas");
+      console.log(error);
     }
-  }
+  };
 
-  
   return (
     <>
       {open && (
         <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex justify-center items-center z-50">
           <div className="bg-white p-5 rounded flex flex-col justify-center items-center gap-5 h-screem">
-            <h4 className=''>
-              Invita a otras empresas a formar parte de Energialy!{" "}
-              <span className="font-bold">{company}</span>
+            <h4 className="">
+              Invita a otras empresas a formar parte de Energialy! <span className="font-bold">{company}</span>
             </h4>
             <div className="w-full">
               <form onSubmit={saveEmail} className="flex gap-2 w-full">
@@ -87,10 +74,7 @@ function EmailModal({ open, handleOpen, id, company }) {
                   placeholder="Correo electrónico"
                   onChange={hanbleEmail}
                 />
-                <button
-                  className="rounded-md bg-secondary-400 font-semibold text-white px-4 py-2"
-                  type="submit"
-                >
+                <button className="rounded-md bg-secondary-400 font-semibold text-white px-4 py-2" type="submit">
                   Agregar
                 </button>
               </form>
@@ -98,15 +82,9 @@ function EmailModal({ open, handleOpen, id, company }) {
             {emails.length > 0 && (
               <div className="grid grid-cols-3 gap-4">
                 {emails.map((email, index) => (
-                  <div
-                    key={index}
-                    className="bg-gray-400 text-gray-50 flex gap-2  text-xs px-2 py-2 rounded-full justify-between items-center"
-                  >
+                  <div key={index} className="bg-gray-400 text-gray-50 flex gap-2  text-xs px-2 py-2 rounded-full justify-between items-center">
                     <span className="font-semibold">{email}</span>
-                    <button
-                      onClick={() => removeEmail(index)}
-                      className="bg-gray-700 w-5 h-5 rounded-full"
-                    >
+                    <button onClick={() => removeEmail(index)} className="bg-gray-700 w-5 h-5 rounded-full">
                       x
                     </button>
                   </div>
@@ -114,20 +92,14 @@ function EmailModal({ open, handleOpen, id, company }) {
               </div>
             )}
             <div className="flex gap-2">
-              <button
-                className="bg-primary-500 py-2 px-4 rounded-md text-white font-bold m-5"
-                onClick={handleStatus}
-              >
+              <button className="bg-primary-500 py-2 px-4 rounded-md text-white font-bold m-5" onClick={handleStatus}>
                 Enviar
               </button>
-              <button
-                className="bg-secondary-500 py-2 px-4 rounded-md text-white font-bold m-5"
-                onClick={handleOpen}
-              >
+              <button className="bg-secondary-500 py-2 px-4 rounded-md text-white font-bold m-5" onClick={handleOpen}>
                 Cerrar
               </button>
             </div>
-            <ToastContainer style={{ marginTop: "100px" }} />
+            <ToastContainer style={{ marginTop: '100px' }} />
           </div>
         </div>
       )}
