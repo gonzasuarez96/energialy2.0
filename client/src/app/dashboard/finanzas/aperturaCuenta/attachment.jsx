3fbac9f41a254161afaddfb4d8ceab745d73062e
juancruzroldan95 <juancruz.roldan19@gmail.com>
2024-03-28 17:54:12 -0300

    Prettier

--- client/src/app/dashboard/finanzas/aperturaCuenta/attachment.jsx
+++ client/src/app/dashboard/finanzas/aperturaCuenta/attachment.jsx
@@ -1,10 +1,10 @@
-"use client";
-import React, { useState } from "react";
-import axios from "axios";
-import getLocalStorage from "@/app/Func/localStorage";
-import { urlProduction } from "@/app/data/dataGeneric";
-import { displayFailedMessage, displaySuccessMessage } from "@/app/components/Toastify";
-import { ToastContainer } from "react-toastify";
+'use client';
+import React, { useState } from 'react';
+import axios from 'axios';
+import getLocalStorage from '@/app/Func/localStorage';
+import { urlProduction } from '@/app/data/dataGeneric';
+import { displayFailedMessage, displaySuccessMessage } from '@/app/components/Toastify';
+import { ToastContainer } from 'react-toastify';
 
 export default function Attachment(props) {
   const [files, setFiles] = useState({
@@ -24,122 +24,111 @@ export default function Attachment(props) {
     compreNeuquino: null,
   });
   const user = getLocalStorage();
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const companyId = user?.company.id;
     const id = {
-      companyId: companyId
-    }
-    console.log('companyIdAttach:', companyId)
-    try{
-      const res = await axios.post(`${urlProduction}/bankAccounts`,id)
-      console.log('res bankAccount:', res)
+      companyId: companyId,
+    };
+    console.log('companyIdAttach:', companyId);
+    try {
+      const res = await axios.post(`${urlProduction}/bankAccounts`, id);
+      console.log('res bankAccount:', res);
       displaySuccessMessage('Solicitud de Apertura de cuenta enviada');
       setTimeout(() => {
         window.location.href = '/dashboard';
       }, 2000);
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
       if (error.response.data.error === 'Company Testing already has a bank account.')
-      displayFailedMessage('La empresa ya tiene una solicitud de Apertura de Cuenta')
+        displayFailedMessage('La empresa ya tiene una solicitud de Apertura de Cuenta');
     }
-   
   };
 
   const fields = [
     {
-      label:
-        "Copia del Estatuto social actualizado y debidamente inscripto en el Registro de Comercio correspondiente.",
-      value: "estatutoSocial",
+      label: 'Copia del Estatuto social actualizado y debidamente inscripto en el Registro de Comercio correspondiente.',
+      value: 'estatutoSocial',
     },
     {
-      label: "Constancia de Inscripción en AFIP.",
-      value: "inscripcionAFIP",
+      label: 'Constancia de Inscripción en AFIP.',
+      value: 'inscripcionAFIP',
     },
     {
-      label: "Constancia de CUIT/CIE/CDI.",
-      value: "cuit",
-    },
-    {
-      label:
-        "Copias de Actas de Reunión de Directorio y Actas de Asamblea (o su equivalente en otro tipo societario) en las cuales se designan autoridades; y Constancia de su inscripción ante el Registro Público correspondiente.",
-      value: "actasReunion",
+      label: 'Constancia de CUIT/CIE/CDI.',
+      value: 'cuit',
     },
     {
       label:
-        "Copia certificada del poder otorgado en favor de los apoderados que los autorice a representar al cliente frente a la institución financiera.",
-      value: "cartaPoder",
+        'Copias de Actas de Reunión de Directorio y Actas de Asamblea (o su equivalente en otro tipo societario) en las cuales se designan autoridades; y Constancia de su inscripción ante el Registro Público correspondiente.',
+      value: 'actasReunion',
     },
     {
       label:
-        "Copia de los 2(dos) últimos estados contables anuales certificados ante el Congreso Profesional correspondiente.",
-      value: "estadosContables",
+        'Copia certificada del poder otorgado en favor de los apoderados que los autorice a representar al cliente frente a la institución financiera.',
+      value: 'cartaPoder',
     },
     {
-      label:
-        "Licencias o autorizaciones otorgadas por organismos gubernamentales (en caso que corresponda).",
-      value: "licencias",
+      label: 'Copia de los 2(dos) últimos estados contables anuales certificados ante el Congreso Profesional correspondiente.',
+      value: 'estadosContables',
     },
     {
-      label:
-        'Certificado de Censo Nacional Agropecuario (solo para actividades alcanzadas según Com."B"11788 del BCRA).',
-      value: "censoAgropecuario",
+      label: 'Licencias o autorizaciones otorgadas por organismos gubernamentales (en caso que corresponda).',
+      value: 'licencias',
     },
     {
-      label:
-        "Registro de accionistas (rúbricas y fojas que demuestren la composición total) o su equivalente en otros tipos societarios.",
-      value: "registroAccionistas",
+      label: 'Certificado de Censo Nacional Agropecuario (solo para actividades alcanzadas según Com."B"11788 del BCRA).',
+      value: 'censoAgropecuario',
+    },
+    {
+      label: 'Registro de accionistas (rúbricas y fojas que demuestren la composición total) o su equivalente en otros tipos societarios.',
+      value: 'registroAccionistas',
     },
     {
       label: 'Certificado de Cumplimiento Censal (Com. "B" 12100 del BCRA).',
-      value: "cumplimientoCensal",
+      value: 'cumplimientoCensal',
     },
     {
       label:
-        "DD.JJ. IVA y comprobante de pago de todos los meses posteriores al cierre del último balance y/o documentación que demuestre el ingreso de fondos del cliente.",
-      value: "ddjjIVA",
+        'DD.JJ. IVA y comprobante de pago de todos los meses posteriores al cierre del último balance y/o documentación que demuestre el ingreso de fondos del cliente.',
+      value: 'ddjjIVA',
     },
     {
-      label: "DD.JJ. Sujeto Obligado (en caso que corresponda).",
-      value: "ddjjSujetoObligado",
+      label: 'DD.JJ. Sujeto Obligado (en caso que corresponda).',
+      value: 'ddjjSujetoObligado',
     },
     {
-      label: "Constancia de UIF (en caso que corresponda).",
-      value: "constanciaUIF",
+      label: 'Constancia de UIF (en caso que corresponda).',
+      value: 'constanciaUIF',
     },
     {
-      label: "Compre Neuquino",
-      value: "compreNeuquino",
+      label: 'Compre Neuquino',
+      value: 'compreNeuquino',
     },
   ];
 
   const uploadFile = async (e, fieldName) => {
     const file = e.target.files[0];
     const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", "energialy_users");
-  
+    formData.append('file', file);
+    formData.append('upload_preset', 'energialy_users');
+
     try {
-      const res = await axios.post(
-        "https://api.cloudinary.com/v1_1/dkgpfpz6o/upload",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-  
+      const res = await axios.post('https://api.cloudinary.com/v1_1/dkgpfpz6o/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
       const fileUrl = res.data.secure_url;
-      console.log("URL del archivo subido:", fileUrl);
-  
+      console.log('URL del archivo subido:', fileUrl);
+
       setFiles((prevFiles) => ({
         ...prevFiles,
         [fieldName]: fileUrl,
       }));
-  
+
       const companyId = user.company.id;
       try {
         const response = await axios.post(`${urlProduction}/documents`, {
@@ -147,15 +136,14 @@ export default function Attachment(props) {
           name: fieldName,
           attachment: fileUrl,
         });
-        console.log("Document uploaded:", response);
+        console.log('Document uploaded:', response);
       } catch (error) {
-        console.error("Error al subir el documento:", error);
+        console.error('Error al subir el documento:', error);
       }
     } catch (error) {
-      console.error("Error al cargar el archivo:", error);
+      console.error('Error al cargar el archivo:', error);
     }
   };
-  
 
   return (
     <main className="flex justify-center items-start w-full  bg-white p-3 shadow ">
@@ -165,37 +153,26 @@ export default function Attachment(props) {
         </label>
         {fields.map((field, index) => (
           <div key={index} className="flex text-left">
-            <label className="text-left border p-4 m-2 font-bold w-[100vh]">
-              {field.label}
-            </label>
+            <label className="text-left border p-4 m-2 font-bold w-[100vh]">{field.label}</label>
             <div className="ml-4 p-2">
-              <input
-                type="file"
-                id={index}
-                accept="image/*,.pdf"
-                onChange={(e) => uploadFile(e, field.value)}
-                className="w-full border rounded "
-              />
+              <input type="file" id={index} accept="image/*,.pdf" onChange={(e) => uploadFile(e, field.value)} className="w-full border rounded " />
             </div>
           </div>
         ))}
         <div className="flex justify-center">
           <button
-            className="px-4 py-2 m-4 font-bold text-white bg-[#191654] rounded hover:bg-secondary-600 transition duration-300"
+            className="px-4 py-2 m-4 font-bold bg-white border-2 border-primary-500 rounded hover:bg-primary-500 hover:text-white transition duration-300"
             type="button"
             onClick={props.handleBack}
           >
             Volver
           </button>
-          <button
-            className="px-4 py-2 m-4 font-bold text-white bg-[#191654] rounded hover:bg-secondary-600 transition duration-300"
-            type="submit"
-          >
+          <button className="px-4 py-2 m-4 font-bold text-white bg-[#191654] rounded hover:bg-secondary-600 transition duration-300" type="submit">
             Enviar
           </button>
         </div>
       </form>
-      <ToastContainer style={{ marginTop: "100px" }} />
+      <ToastContainer style={{ marginTop: '100px' }} />
     </main>
   );
 }
--- client/src/app/dashboard/finanzas/aperturaCuenta/data.jsx
+++ client/src/app/dashboard/finanzas/aperturaCuenta/data.jsx
@@ -1,52 +1,49 @@
-"use client";
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import axios from "axios";
-import {
-  displayFailedMessage,
-  displaySuccessMessage,
-} from "@/app/components/Toastify";
-import { ToastContainer } from "react-toastify";
-import getLocalStorage from "@/app/Func/localStorage";
-import { urlProduction } from "@/app/data/dataGeneric";
+'use client';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { displayFailedMessage, displaySuccessMessage } from '@/app/components/Toastify';
+import { ToastContainer } from 'react-toastify';
+import getLocalStorage from '@/app/Func/localStorage';
+import { urlProduction } from '@/app/data/dataGeneric';
 
 export default function Data(props) {
   // Estados Locales
   const [envioExitoso, setEnvioExitoso] = useState(false);
-  const [error, setError] = useState("");
-  const [businessName, setBusinessName] = useState("");
-  const [fiscalAdress, setFiscalAdress] = useState("");
-  const [cuit, setCuit] = useState("");
-  const [companyEmail, setCompanyEmail] = useState("");
+  const [error, setError] = useState('');
+  const [businessName, setBusinessName] = useState('');
+  const [fiscalAdress, setFiscalAdress] = useState('');
+  const [cuit, setCuit] = useState('');
+  const [companyEmail, setCompanyEmail] = useState('');
   const [legalManager, setLegalManager] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    position: "",
-    phoneNumber: "",
+    firstName: '',
+    lastName: '',
+    email: '',
+    position: '',
+    phoneNumber: '',
   });
   const user = getLocalStorage();
 
   const handleChange = (e) => {
     const { id, value } = e.target;
 
-    if (id === "legalManager") {
+    if (id === 'legalManager') {
       setLegalManager({
         ...legalManager,
         [e.target.name]: value,
       });
     } else {
       switch (id) {
-        case "businessName":
+        case 'businessName':
           setBusinessName(value);
           break;
-        case "fiscalAdress":
+        case 'fiscalAdress':
           setFiscalAdress(value);
           break;
-        case "cuit":
+        case 'cuit':
           setCuit(value);
           break;
-        case "companyEmail":
+        case 'companyEmail':
           setCompanyEmail(value);
           break;
         default:
@@ -56,7 +53,7 @@ export default function Data(props) {
   };
 
   const handleNextButtonClick = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const companyId = user?.company.id;
     const accountData = {
       businessName,
@@ -78,22 +75,18 @@ export default function Data(props) {
       !legalManager.position ||
       !legalManager.phoneNumber
     ) {
-      setError("Completa todos los campos");
+      setError('Completa todos los campos');
       return;
     }
 
-    // Envío de datos
     try {
-      const res = await axios.put(
-        `${urlProduction}/companies/${companyId}`,
-        accountData
-      );
-      console.log("resData server:", res);
+      const res = await axios.put(`${urlProduction}/companies/${companyId}`, accountData);
+      console.log('resData server:', res);
       setEnvioExitoso(true);
-      displaySuccessMessage("Datos enviados con éxito, presiona siguiente para subir la documentación");
-      props.handleNext(); // Ejecutar siguiente si el envío de datos fue exitoso
+      displaySuccessMessage('Datos enviados con éxito, presiona siguiente para subir la documentación');
+      props.handleNext();
     } catch (error) {
-      console.log("errorData:", error);
+      console.log('errorData:', error);
       displayFailedMessage(error.response.data.error);
     }
   };
@@ -101,14 +94,10 @@ export default function Data(props) {
   return (
     <main className="flex justify-center items-start w-full  bg-white p-3 shadow">
       <div className="w-full text-center">
-        <h2 className="p-4 border-b-2 border-gray-300 font-bold">
-          Solicitud de Apertura de Cuenta
-        </h2>
+        <h2 className="p-4 border-b-2 border-gray-300 font-bold">Solicitud de Apertura de Cuenta</h2>
 
         <div>
-          <label className="block mb-2 bg-[#f7f7f7] py-4 pl-7 mt-4 font-bold border-l-4 border-primary-500 text-left">
-            Tu Empresa
-          </label>
+          <label className="block mb-2 bg-[#f7f7f7] py-4 pl-7 mt-4 font-bold border-l-4 border-primary-500 text-left">Tu Empresa</label>
           <div className="mt-4 text-left">
             <input
               type="text"
@@ -215,22 +204,18 @@ export default function Data(props) {
           </div>
           <div className="flex justify-center">
             <button
-              className="px-4 py-2 m-4 font-bold text-white bg-[#191654] rounded hover:bg-secondary-600 transition duration-300"
+              className="px-4 py-2 m-4 font-bold text-white bg-primary-500 rounded hover:bg-secondary-600 transition duration-300"
               type="button"
-              onClick={handleNextButtonClick} // Al hacer clic en este botón, se ejecutará handleNextButtonClick
+              onClick={handleNextButtonClick}
             >
               Siguiente
             </button>
           </div>
-          {error && (
-            <div className="flex justify-center text-danger mt-2 mb-2">
-              {error}
-            </div>
-          )}
+          {error && <div className="flex justify-center text-danger mt-2 mb-2">{error}</div>}
         </div>
       </div>
       {/* ToastContainer y UploadthingButton van aquí si es necesario */}
-      <ToastContainer style={{ marginTop: "100px" }} />
+      <ToastContainer style={{ marginTop: '100px' }} />
     </main>
   );
 }
