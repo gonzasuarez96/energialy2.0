e90046cbcc11aeadeadb684741f5293da37515fb
juancruzroldan95 <juancruz.roldan19@gmail.com>
2024-03-28 17:53:50 -0300

    Set `BASE_URL` environment variable

--- client/src/app/data/dataGeneric.js
+++ client/src/app/data/dataGeneric.js
@@ -27,6 +27,4 @@ export const duration = ['Menos de una semana', 'Menos de un mes', 'De 1 a 3 mes
 
 export const etapa = ['Upstream', 'Midstream', 'Downstream'];
 
-// export const urlProduction = "http://localhost:3001";
-
-export const urlProduction = 'https://api-energialy.vercel.app';
+export const urlProduction = process.env.NEXT_PUBLIC_BASE_URL;
